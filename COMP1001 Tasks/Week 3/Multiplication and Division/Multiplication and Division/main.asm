f08ba36a252af9d31dc54f93a8bf0c660ea3b1d9
seanb-313 <18nbautistas@dhsb.org>
2025-10-06 19:29:20 +0100

    dworking

--- COMP1001 Tasks/Week 3/Multiplication and Division/Multiplication and Division/main.asm
+++ COMP1001 Tasks/Week 3/Multiplication and Division/Multiplication and Division/main.asm
@@ -4,38 +4,29 @@
 ExitProcess PROTO, dwExitCode: DWORD
 
 .data
-	zero DWORD 0
 	fifty DWORD 50;
 	score1 DWORD 35;
 	max1 DWORD 60
 	score2 DWORD 45
-	max2 DWORD 55
-	numerator DWORD ?
-	denominator DWORD ?
-	quotient DWORD ?
-	remainder DWORD ?
+	max2 BYTE 55
+	divisor DWORD ?
 
 
 .code
 
 main PROC
-	mov edx, zero
-	mov eax, score1
-	mul fifty
-	mul max2
-	mov ebx, score2
-	mul fifty
-	mul max1
-	add eax, ebx
-	mov numerator, eax
+	; make divisor first
 	mov eax, max1
 	mul max2
-	mov denominator, eax
-	mov eax, numerator
-	div denominator
-	mov quotient, eax
-	mov remainder, edx
-
+	mov divisor, eax ;3300 expected
+	mov eax, score1 
+	mul max2
+	mov ecx, eax
+	mov eax, score2 
+	mul max1
+	add eax, ecx
+	mul fifty
+	div divisor
 
 		INVOKE ExitProcess, 0 ; call exit function
  
