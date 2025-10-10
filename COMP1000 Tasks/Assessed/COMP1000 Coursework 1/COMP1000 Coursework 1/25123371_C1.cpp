b4423ca5f4f0dc0d95a6605aea9f23199fe01d22
smbautista07 <sean.bautista@students.plymouth.ac.uk>
2025-10-10 12:36:27 +0100

    Week 3 progress
    Started assessment 1

---
+++ COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
@@ -0,0 +1,17 @@
+//Name - Sean Bautista
+//ID - 25123371
+
+#include <iostream>
+using namespace std;
+const int studentID = 25123371;
+
+void task1()
+{
+	
+	cout << studentID;
+}
+int main()
+{
+	task1();
+	return 0;
+}
