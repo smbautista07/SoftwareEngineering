799db69d4f5cbda03af7a0c16d986ada10a262dd
seanb-313 <18nbautistas@dhsb.org>
2025-10-29 23:31:35 +0000

    COMP1000 Assessment update
    Completed tasks 1-9. Clarification needed on task 10 and whether <string> header is allowed.

--- COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
+++ COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
@@ -1,8 +1,8 @@
 //Name - Sean Bautista
 //ID - 25123371
 
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 const int studentID = 25123371;
 
@@ -153,8 +153,48 @@ int int32Input(string promptMessage)	//ensures a 32 bit integer is input without
 	return stringToInt(input);
 }
 
+int timesCalled() //function containing static variable defined for task 8
+{
+	static int calls = 0; //initialise a static variable with the value of 0. This does not affect it again since a static variable does not need to be initialised again.
+	calls++; //every time this function is called, the value increments. 
+	return calls; //return value stored in the static variable.
+}
 
-void task1()
+double averagePositive() {
+	// Debugging task 9
+	// Function: Computes average of positive numbers until -1 is entered
+	// Note: Code contains multiple logical errors for you to fix
+
+	double sum = 0.0;
+	int count = 0;
+	int num = 0;
+
+	do {
+		cin >> num;
+
+		//Bug 1: Negative numbers are added to the sum when only positive numbers should be accepted into the average.
+		//Fix: Use an if statement to ensure the number is above 0 before making any changes.
+		//Bug 2: Negative numbers increase the count despite being invalid.
+		//Fix: Include the count increment in the (if num > 0) statement.
+		if (num > 0) //Note: 0 is not a positive number so it is not included in the tally.	
+		{
+			sum = sum + num;
+			count = count + 1;  
+		}
+	} while (num != -1);
+
+	//Bug 3: Integer division used, which removes any decimal part of the number.
+	//Fix: Change sum data type into double. This results in double/integer calculation so that no data is lost
+	return sum / count;
+
+	//I tested inputs by outputting value stored by sum after every input and average at the end.
+	//All positive: the sum updated every input, the output was the average of all numbers.
+	/*	All negative : The sum and average did not change after any input
+		The value returned is 0.0 since the value in sum hasn't changed since initialisation.*/
+	//Mixed: The positive numbers affected the sum value while the negatives (and 0) had no effect on the average.
+}	
+
+void task1() //add two inputs
 {
 	//Initialises two integer variables using user integer input twice.
 	int num1 = int32Input("Input one number");  //Second to last digit of studentID = 7
@@ -164,15 +204,131 @@ void task1()
 	cout << num1 << " + " << num2 << " = " << num1 + num2; //Would output 8 (7+1) using studentID
 }
 
-void task2()
+void task2() //multilply input by digit in studentID
 {
-	int num1 = int32Input("Input one number");
+	int num1 = int32Input("Input one number"); //prompts the user for an integer input
+	int num2 = 7; //second to last number of studentID
+	cout << num1 << " x " << num2 << " = " << num1 * num2; //returns a message showing the multiplication process and result.
 }
 
+void task3()
+{
+	int loopCount = max(10, studentID % 20); //initialises an integer variable with the higher value between 10 and studentID%20
+	//loops from 1 to that value inclusive since the assignment brief specifically said that the end value is either 10 or studentID%20
+	for (int i = 1;i <= loopCount; i++)
+	{
+		cout << i << "\n";  //outputs the current number and starts a new line for clarity.
+	}
+}
+
+void task4() //odd or even check
+{
+	int num = studentID; //variable for maintenance
+	if (num % 2 == 0) //if studentID mod 2 operator ressults in 0, the number is even
+	{
+		cout << num << " is even"; //output number and result
+	}
+	else //otherwise it's odd
+	{
+		cout << num << " is odd";
+	}
+}
+
+void task5() //switch case statements
+{
+	int num = studentID % 7 + 1; //initialises integer variable with studentID mod 7 + 1 
+	switch (num) //outputs a different result based on the value stored in the variable
+	{
+	case 1: //if the value stored in the variable is 1:
+		cout << "Monday"; //output Monday, which is the first day of the week
+		break;
+	case 2: //do the same for the remaining days 2-7
+		cout << "Tuesday";
+		break;
+	case 3:
+		cout << "Wednesday";
+		break;
+	case 4:
+		cout << "Thursday";
+		break;
+	case 5:
+		cout << "Friday";
+		break;
+	case 6:
+		cout << "Saturday";
+		break;
+	case 7:
+		cout << "Sunday";
+		break;
+	default: //if the value stored isn't associated with a day of the week,
+		cout << "Not a day of the week"; //tell the user it isn't associated with a day
+	}
+}
+
+void task6()
+{
+	int arr[5] = {51,3,studentID % 50,23,44}; //initialise an array with 5 values
+	cout << arr[2]; //outputs the third element in the array
+}
+
+void task7()
+{
+	float distance; //initialise a float variable to store result, since it's unlikely to be an integer
+	int point1[2]{51, 634};
+	int point2[2] = {7, 1};
+
+	//calculates euclidean distance pythagorean theorem: root(a^2 + b^2) = c, where a is the difference in x-coordinates, b is the difference in y-coordinates, and c is the euclidean distance
+	distance = sqrt((point1[0] - point2[0]) ^ 2 + (point1[1] - point2[1]) ^ 2);
+	
+	//tells the user the points and results
+	cout <<"Euclidean distance between "<< "(" << point1[0] << "," << point1[1] << ")" << " and " "(" << point2[0] << "," << point2[1] << ")" " = " << distance; 
+}
+
+void task8()
+{
+	for (int i = 0; i < studentID%5; i++) //calls the function studentID%5 times using a loop
+	{
+		timesCalled();
+	}
+
+	//outputs the value of the static variable studentID%5 + 1 when including the call below
+	cout << timesCalled();
+}
+
+void task9()
+{
+	cout << averagePositive(); //check averagePositive() function for debugging task changes.
+	cin.ignore(); //Clears input stream to prevent issues with task 10, which uses getline.
+}
+
+int demonstration = studentID;
+void globalOutputDemonstration()
+{
+
+	cout << "value of this variable from another function" << demonstration << "\n";
+}
+
+void task10() //unsure if this is comparing global with static global or global with local. For now it is just comparing global with local. Needs clarification.
+{
+	cout << "A global variable with the value " <<demonstration<<"has been declared."<<"\n";
+	cout << "Try to overwrite it: ";
+	cin >> demonstration;
+	cout << "Value you output: " << demonstration;
+	globalOutputDemonstration(); //shows accessibility to other functions
+	cout << demonstration;
+	
+}
 
 int main()
 {
-	task1();
-	
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	//task5();
+	//task6();
+	//task8();
+	//task9();
+	//task10();
 	return 0;
 }
