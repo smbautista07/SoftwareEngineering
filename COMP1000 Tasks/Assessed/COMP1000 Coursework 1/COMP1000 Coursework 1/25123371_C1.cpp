65f65513ba802909e3b9279ba9482e26d31cc7c9
seanb-313 <18nbautistas@dhsb.org>
2025-10-29 18:54:13 +0000

    Input validation fix
    cin with the extraction operator has space characters as a delimiter by default, which caused problems in my code: Mainly that it caused my input validation system to treat strings seperated with spacebars as different inputs to be checked seperately, resulting in invalid inputs to be validated e.g "32321983123 247"would disqualify the first number stated and qualify the "247", which is not exactly what the user has input. This has been fixed using getline(cin,var) but since it also includes spaces in the input I also needed to create a function to trim leading and trailing space characters.

--- COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
+++ COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
@@ -2,6 +2,7 @@
 //ID - 25123371
 
 #include <iostream>
+#include <string>
 using namespace std;
 const int studentID = 25123371;
 
@@ -12,21 +13,12 @@ bool negativeStringCheck(string stringNumber)
 
 bool stringIntCheck(string stringNumber) //checks whether the string value stores an integer
 {
-	
-	//if user input is too large - max 32 bit integer length as a string is 10 (+ 1 if negative sign), the input is invalid
-	/*if (stringNumber.length() > 10 + negativeStringCheck(stringNumber)) //bools can be implicity converted to integer values 0 or 1, so 11 character inputs are accepted as long as the input is negative
-	{
-		cout << "input is too long" << "\n"; //informs the user of the error
-		return false;
-	}*/
-	
-	//checks if the string any characters which isn't a negative sign or digit.
 	for (int i = negativeStringCheck(stringNumber); i < stringNumber.length(); i++) //loops through entire string, ignoring the first character if it's a negative sign
 	{
 		if (isdigit(stringNumber[i]) == false) 
 		{
-			cout << "input contains a character which is neither a digit nor negative sign."; //informs the user what's wrong with the input
-			return false; 
+			cout << "Input contains a character which is neither a digit nor negative sign." << "\n"; //informs the user what's wrong with the input
+			return false;
 		}
 	}
 	return true; // if both checks haven't found anything wrong, return true
@@ -51,6 +43,7 @@ int stringToInt(string stringNumber)
 bool stringInt32Check(string stringNumber)
 {
 	bool isNegative = negativeStringCheck(stringNumber);
+	
 	if (stringNumber.length() > 10 + isNegative) //checks if the input is too long (>10 when excluding negative sign). Addressed first in case of absurdly long input
 	{
 		cout << "Input is too large to be a 32 bit integer (up to 10 characters long, or 11 for negative numbers)" <<"\n";
@@ -61,6 +54,8 @@ bool stringInt32Check(string stringNumber)
 	{
 		return false;
 	}
+
+	//The inputs remaining are integers which are 10 or less digits long
 
 	//if the string is less than 10 digits long (excluding the negative sign) it is always valid. Most inputs are this
 	if (stringNumber.length() < 10 + isNegative)
@@ -93,28 +88,56 @@ bool stringInt32Check(string stringNumber)
 	return true;
 }
 
+string trim(string input) //Removes the spaces before and after an input
+{
+	//declare two variables which determine the start and end of the input. 
+	int start = 0;
+	int end = 0;
+
+	//iterate from the start of the input
+	for (int x = 0; x < input.length(); x++)
+	{
+		if (isspace(input[x]) == false)
+		{
+			start = x; //start variable is set to the index of the first non-space character
+			break;
+		}
+	}
+	//iterate from end of input to the start. 
+	for (int y = input.length()-1; y > start-1; y--)
+	{
+		if (isspace(input[y]) == false)
+		{
+			end = y; //end variable is set to the index of the last non-space character.
+			break;
+		}
+	}
+	int length = end - start + 1; //length of the string which will be returned
+	return input.substr(start, length);
+}
+
 int int32Input(string promptMessage)	//ensures a 32 bit integer is input without crashing from invalid data types
 {
 	cout << promptMessage << "\n"; //lets the user know that they have to input something, starts a new line for input
 
 	//store user input as a string to avoid type error
 	string input;
+
 	//loops until an input is valid
 	do
 	{
-		//reset loop flag
-		bool validInput = true; //initialises flag which informs the program to loop if nothing wrong is found with the input
-
 		//user input
-		cin >> input;	
+		getline(cin, input); //input method which, unlike cin with an extraction operator, does not use the space as a delimiter.
+		
+		if (input.length() == 0) //prevent empty inputs
+		{
+			cout << "Empty input. Try again" <<"\n";
+			continue;
+		}
 
-		//checks if an integer is stored in string
+		input = trim(input); //removes leading and trailing spaces
 	} while (stringInt32Check(input) == false);
-	
 	return stringToInt(input);
-
-	
-	return 0;
 }
 
 
@@ -125,12 +148,18 @@ void task1()
 	int num2 = int32Input("Input another"); //Last digit of studentID = 1
 
 	//outputs the sum of the two inputs. 
-	cout << num1+num2; //Would output 8 (7+1) using studentID
+	cout << num1 << " + " << num2 << " = " << num1 + num2; //Would output 8 (7+1) using studentID
 }
+
+void task2()
+{
+	int num1 = int32Input("Input one number");
+}
+
+
 int main()
 {
-	
-	cout << int32Input("test");
+	task1();
 	
 	return 0;
 }
