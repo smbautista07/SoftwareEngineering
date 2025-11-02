6185e0c9a0a253904f4c31ee0d4b11b190e44cfd
seanb-313 <18nbautistas@dhsb.org>
2025-11-02 19:32:14 +0000

    COMP1000 Assessment
    Cleanup and output clarity. Ready for submission

--- COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
+++ COMP1000 Tasks/Assessed/COMP1000 Coursework 1/COMP1000 Coursework 1/25123371_C1.cpp
@@ -153,11 +153,127 @@ int int32Input(string promptMessage)	//ensures a 32 bit integer is input without
 	return stringToInt(input);
 }
 
+void task1() //add two inputs
+{
+	cout << "Task 1: Add two integer inputs (studentID ver in coments)" << "\n";
+
+	//Initialises two integer variables using user integer input twice.
+	int num1 = int32Input("Input one number");  //Second to last digit of studentID = 7
+	int num2 = int32Input("Input another"); //Last digit of studentID = 1
+
+	//long was used to prevent overflow
+	long sum = num1 + num2;
+	//outputs the sum of the two inputs. 
+	cout << num1 << " + " << num2 << " = " << sum << "\n"; //Would output 8 (7+1) using studentID
+}
+
+void task2() //multilply input by digit in studentID
+{
+	cout << "Task 2: Multiply two numbers together (one is studentID's first digit: 2)" << "\n";
+
+	int num1 = 32; //prompts the user for an integer input
+	int num2 = 2; //second to last number of studentID
+	cout << num1 << " x " << num2 << " = " << num1 * num2 << "\n"; //returns a message showing the multiplication process and result.
+}
+
+void task3() 
+{
+	cout << "Task 3: Output every number from 1 to 10 or studentID%20 inclusive, whichever is higher"<< "\n";
+
+	int loopCount = max(10, studentID % 20); //initialises an integer variable with the higher value between 10 and studentID%20
+	cout << "Chosen value: " << loopCount << "\n";
+	//loops from 1 to that value inclusive since the assignment brief specifically said that the end value is either 10 or studentID%20
+	for (int i = 1;i <= loopCount; i++)
+	{
+		cout << i << "\n";  //outputs the current number and starts a new line for clarity.
+	}
+}
+
+void task4() 
+{
+	cout << "Task 4: Odd or even check" << "\n"; 
+
+	int num = studentID; //variable for maintenance
+	if (num % 2 == 0) //if studentID mod 2 operator ressults in 0, the number is even
+	{
+		cout << num << " is even"; //output number and result
+	}
+	else //otherwise it's odd
+	{
+		cout << num << " is odd";
+	}
+	cout << "\n";
+}
+
+void task5() //switch case statements
+{
+	cout << "Task 5: Switch case for each day" << "\n";
+	int num = studentID % 7 + 1; //initialises integer variable with studentID mod 7 + 1 
+
+	cout << "Chosen value: " << num << "\n";
+	switch (num) //outputs a different result based on the value stored in the variable
+	{
+	case 1: //if the value stored in the variable is 1:
+		cout << "Monday"; //output Monday, which is the first day of the week
+		break;
+	case 2: //do the same for the remaining days 2-7
+		cout << "Tuesday";
+		break;
+	case 3:
+		cout << "Wednesday";
+		break;
+	case 4:
+		cout << "Thursday";
+		break;
+	case 5:
+		cout << "Friday";
+		break;
+	case 6:
+		cout << "Saturday";
+		break;
+	case 7:
+		cout << "Sunday";
+		break;
+	default: //if the value stored isn't associated with a day of the week,
+		cout << "Not a day of the week"; //tell the user it isn't associated with a day
+	}
+	cout << "\n";
+}
+
+void task6()
+{
+	cout << "Task 6: Declare array of 5 elements and output the third" << "\n";
+	int arr[5] = {51,3,studentID % 50,23,44}; //initialise an array with 5 values
+	cout << "Third element in the array: " << arr[2] << "\n"; //outputs the third element in the array
+}
+
+void task7()
+{
+	double distance; //initialise a float variable to store result, since it's unlikely to be an integer
+	int point1[2]{51, 634};
+	int point2[2] = {7, 1};
+
+	//calculates euclidean distance pythagorean theorem: root(a^2 + b^2) = c, where a is the difference in x-coordinates, b is the difference in y-coordinates, and c is the euclidean distance
+	distance = sqrt((point1[0] - point2[0]) ^ 2 + (point1[1] - point2[1]) ^ 2);
+	
+	//tells the user the points and results
+	cout <<"Euclidean distance between "<< "(" << point1[0] << "," << point1[1] << ")" << " and " "(" << point2[0] << "," << point2[1] << ")" " = " << distance; 
+}
+
 int timesCalled() //function containing static variable defined for task 8
 {
-	static int calls = 0; //initialise a static variable with the value of 0. This does not affect it again since a static variable does not need to be initialised again.
+	static int calls = 0; //initialise a static variable with the value of 0. Static variables will not be reinitialised so the value will not be reset.
 	calls++; //every time this function is called, the value increments. 
-	return calls; //return value stored in the static variable.
+	cout << "This function has been called " << calls << " times";
+	return calls; //return value stored in the static variable. This value will remain even after the function ends.
+}
+void task8()
+{
+	cout << "Task 8: Track how many times a function is called using a static local variable" << "\n";
+	for (int i = 0; i < studentID%5+1; i++) //calls the function studentID%5 times using a loop
+	{
+		timesCalled();
+	}
 }
 
 double averagePositive() {
@@ -193,153 +309,48 @@ double averagePositive() {
 		The value returned is 0.0 since the value in sum hasn't changed since initialisation.*/
 	//Mixed: The positive numbers affected the sum value while the negatives (and 0) had no effect on the average.
 }	
-
-void task1() //add two inputs
-{
-	//Initialises two integer variables using user integer input twice.
-	int num1 = int32Input("Input one number");  //Second to last digit of studentID = 7
-	int num2 = int32Input("Input another"); //Last digit of studentID = 1
-
-	//outputs the sum of the two inputs. 
-	cout << num1 << " + " << num2 << " = " << num1 + num2; //Would output 8 (7+1) using studentID
-}
-
-void task2() //multilply input by digit in studentID
-{
-	int num1 = int32Input("Input one number"); //prompts the user for an integer input
-	int num2 = 7; //second to last number of studentID
-	cout << num1 << " x " << num2 << " = " << num1 * num2; //returns a message showing the multiplication process and result.
-}
-
-void task3()
-{
-	int loopCount = max(10, studentID % 20); //initialises an integer variable with the higher value between 10 and studentID%20
-	//loops from 1 to that value inclusive since the assignment brief specifically said that the end value is either 10 or studentID%20
-	for (int i = 1;i <= loopCount; i++)
-	{
-		cout << i << "\n";  //outputs the current number and starts a new line for clarity.
-	}
-}
-
-void task4() //odd or even check
-{
-	int num = studentID; //variable for maintenance
-	if (num % 2 == 0) //if studentID mod 2 operator ressults in 0, the number is even
-	{
-		cout << num << " is even"; //output number and result
-	}
-	else //otherwise it's odd
-	{
-		cout << num << " is odd";
-	}
-}
-
-void task5() //switch case statements
-{
-	int num = studentID % 7 + 1; //initialises integer variable with studentID mod 7 + 1 
-	switch (num) //outputs a different result based on the value stored in the variable
-	{
-	case 1: //if the value stored in the variable is 1:
-		cout << "Monday"; //output Monday, which is the first day of the week
-		break;
-	case 2: //do the same for the remaining days 2-7
-		cout << "Tuesday";
-		break;
-	case 3:
-		cout << "Wednesday";
-		break;
-	case 4:
-		cout << "Thursday";
-		break;
-	case 5:
-		cout << "Friday";
-		break;
-	case 6:
-		cout << "Saturday";
-		break;
-	case 7:
-		cout << "Sunday";
-		break;
-	default: //if the value stored isn't associated with a day of the week,
-		cout << "Not a day of the week"; //tell the user it isn't associated with a day
-	}
-}
-
-void task6()
-{
-	int arr[5] = {51,3,studentID % 50,23,44}; //initialise an array with 5 values
-	cout << arr[2]; //outputs the third element in the array
-}
-
-void task7()
-{
-	float distance; //initialise a float variable to store result, since it's unlikely to be an integer
-	int point1[2]{51, 634};
-	int point2[2] = {7, 1};
-
-	//calculates euclidean distance pythagorean theorem: root(a^2 + b^2) = c, where a is the difference in x-coordinates, b is the difference in y-coordinates, and c is the euclidean distance
-	distance = sqrt((point1[0] - point2[0]) ^ 2 + (point1[1] - point2[1]) ^ 2);
-	
-	//tells the user the points and results
-	cout <<"Euclidean distance between "<< "(" << point1[0] << "," << point1[1] << ")" << " and " "(" << point2[0] << "," << point2[1] << ")" " = " << distance; 
-}
-
-void task8()
-{
-	for (int i = 0; i < studentID%5; i++) //calls the function studentID%5 times using a loop
-	{
-		timesCalled();
-	}
-
-	//outputs the value of the static variable studentID%5 + 1 when including the call below
-	cout << timesCalled();
-}
-
 void task9()
 {
-	cout << averagePositive(); //check averagePositive() function for debugging task changes.
+	cout << "Task 9 debugging task: Input positive integers until -1 is input to show the average. Annotations in averagePositive" << "\n";
+	cout << averagePositive() << "\n"; //check averagePositive() function for debugging task changes.
 	cin.ignore(); //Clears input stream to prevent issues with task 10, which uses getline.
+
 }
 
-int demonstration = studentID;
-void globalOutputDemonstration()
+static string stat_glob = "static global with studentID 25123371"; //global static variable. Visible to all functions in this file but not visible in other files.
+string glob = "global with studentID 25123371"; //global variable, visible to all linked files and functions.
+
+void func1() 
 {
-
-	cout << "value of this variable from another function" << demonstration << "\n";
-}
-
-
-
-static string stat_glob = "static global"; //global static variable. Visible to all functions in this file but not visible in other files.
-string glob = "global"; //global variable, visible to all linked files and functions.
-
-void func1()
-{
-	cout << stat_glob << "\n";
+	cout << "This is being accessed from an another function";
+	cout << stat_glob << "\n"; 
 	cout << glob << "\n";
 }
-
 void func2()
 {
+	cout << "This is being accessed from an another function";
 	cout << stat_glob << "\n";
 	cout << glob << "\n";
 }
 void task10() 
 {
+	cout << "Task 10: Static and global demonstration" << "\n";
+	//These two functions demonstrate both global and static global variables can be used by any procedure in the same file.
 	func1();
 	func2();
+	cout << "These two variables are accessible anywhere in this file, but the static global variable is not visible outside the file.";
 }
 
 int main()
-{
-	//task1();
-	//task2();
-	//task3();
-	//task4();
-	//task5();
-	//task6();
-	//task8();
-	//task9();
+{	
+	task1();
+	task2();
+	task3();
+	task4();
+	task5();
+	task6();
+	task8();
+	task9();
 	task10();
 	return 0;
 }
