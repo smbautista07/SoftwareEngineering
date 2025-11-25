cf459ae656f98e730ba8af243fbdab522f6e71b4
smbautista07 <seanmabautista@gmail.com>
2025-11-25 13:03:39 +0000

    Collision detection
    AABB collision detection implementation for practice

--- COMP1004 Tasks/Game practice/render.js
+++ COMP1004 Tasks/Game practice/render.js
@@ -1,17 +1,77 @@
-class pongBall
+"use strict";
+
+class rectObj
 {
-    constructor(width, height)
+    constructor(x, y, width = 0, height = 0)
     {
-        this.x = 0;
-        this.y = 0;
-        this.xSpeed = 10;
-        this.ySpeed = 10;
+        this.x = x;
+        this.y = y;
         this.width = width;
         this.height = height;
+    }
+}
+
+class rectCollider extends rectObj
+{
+    constructor(x, y, width, height)
+    {
+        super(x,y,width, height);
+    }
+    
+    isCollidingWith(otherObj)
+    {
+        let xLeft = this.x;
+        let xRight = this.x+this.width; //may need to use trig for rotated shapes.
+        let yTop = this.y; //same for this
+        let yBottom = this.y + this.height;
+        let theirXLeft = otherObj.x;
+        let theirXRight = otherObj.x+otherObj.width;
+        let theirYTop = otherObj.y;
+        let theirYBottom = otherObj.y+otherObj.height; 
+         if (xRight< theirXLeft)
+         {
+             console.log("ball is to the left");
+         }
+        if (xLeft > theirXRight)
+         {
+             console.log("ball is to the right");
+         }
+         if (xRight> theirXLeft && xLeft < theirXRight)
+         {
+             console.log("ball is to the x aligned");
+             if (yTop < theirYBottom && yBottom > theirYTop) // y axis aligned
+            {
+                console.log("COLLIDING");
+            }
+         }
+         
+
+        // if (xLeft < theirXRight || xRight > theirXLeft) //x axis aligned
+        // {
+        //         console.log("x axis aligned");
+        //     if (yTop < theirYBottom || yBottom > theirYTop) // y axis aligned
+        //     {
+        //         console.log("COLLIDING");
+        //     }
+        // }
+    }
+}
+
+class pongBall extends rectCollider
+{
+    constructor()
+    {
+        super(20,20,20,20)
+        this.xSpeed = 5;
+        this.ySpeed = 5;
+        this.xPrev;
+        this.yPrev;
     }
     
     update()
     {
+        this.xPrev = this.x;
+        this.yPrev = this.y;   
         this.x += this.xSpeed;
         this.y += this.ySpeed;
         if (this.x > 960 || this.x < 0)
@@ -23,8 +83,9 @@ class pongBall
             this.ySpeed *= -1;
         }
     }
-}
 
+
+}
 
 document.addEventListener('DOMContentLoaded', loadTools);
 
@@ -49,18 +110,24 @@ function drawBackground()
 
 
 
-const ball = new pongBall(20,20);
+const ball = new pongBall();
+const wall = new rectCollider(480,0,30,240);
+ctx.fillStyle = "yellow";
+ctx.fillRect(wall.x, wall.y, wall.width, wall.height);
+
 render();
-function render()
+function render()   
 {
     ctx.fillStyle = "yellow";
+    ctx.clearRect(ball.xPrev, ball.yPrev, ball.width,ball.height)
     ctx.fillRect(ball.x,ball.y,ball.width,ball.height);
 }
 
 function update()
 {
     ball.update();
-    console.log("update!");
+    ball.isCollidingWith(wall);
+    //console.log("update!");
     render();
 }
 
